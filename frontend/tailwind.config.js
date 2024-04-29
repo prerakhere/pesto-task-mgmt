a7b9382574ec574f2466cc81bad0d4a27bb01494
Prerak Jain <prerakjain1007@gmail.com>
2024-04-29 20:36:47 +0530

    fe: tailwind housekeeping stuff

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xxs: "320px",
+      subxs: "360px",
+      xs: "485px",
+      ...defaultTheme.screens,
+    },
     extend: {
       keyframes: {
         overlayShow: {
@@ -20,7 +28,20 @@ export default {
         overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
         contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
+      fontSize: {
+        "2xs": "0.7rem",
+        "3xs": "0.65rem",
+        "4xs": "0.62rem",
+        "over-xs": "0.8rem",
+        "sub-sm": "0.85rem",
+        "sub-base": "0.9rem",
+        "over-base": "1.0625rem",
+        "1.5xl": "1.375rem",
+        "2.5xl": "1.7rem",
+        "3.5xl": "2rem",
+        "4.5xl": "2.7rem",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
