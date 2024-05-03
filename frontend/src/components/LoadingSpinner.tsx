8d052e183784bfd09008878c72c1b3ccb4a658a6
Prerak Jain <prerakjain1007@gmail.com>
2024-05-03 21:08:08 +0530

    fe: added loading spinner

---
+++ frontend/src/components/LoadingSpinner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/LoadingSpinner.css";
+
+export default function LoadingSpinner() {
+  return (
+    <div>
+      <div className="col-sm-2">
+        <div className="sp sp-circle"></div>
+      </div>
+    </div>
+  );
+}
