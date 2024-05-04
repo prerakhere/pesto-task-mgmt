6ad97b17437fe25fce673c30a3de21cfc34a4073
Prerak Jain <prerakjain1007@gmail.com>
2024-05-04 12:03:50 +0530

    be: changed method return types

--- backend/src/repositories/interfaces/ITaskRepository.ts
+++ backend/src/repositories/interfaces/ITaskRepository.ts
@@ -2,7 +2,7 @@ import Task from "../response-contracts/ITask";
 
 export default interface ITaskRepository {
   getAllTasks(userId: string): Promise<Task[]>;
-  updateTask(taskId: string, updatedTask: Task): Promise<boolean>;
-  deleteTask(taskId: string): Promise<boolean>;
   createTask(userId: string, newTask: Task): Promise<Task>;
+  updateTask(taskId: string, updatedTask: Task): Promise<Task>;
+  deleteTask(taskId: string): Promise<void>;
 }
--- backend/src/repositories/interfaces/IUserRepository.ts
+++ backend/src/repositories/interfaces/IUserRepository.ts
@@ -2,5 +2,5 @@ import User from "../response-contracts/IUser";
 
 export default interface IUserRepository {
   getUserId(emailId: string): Promise<{ id: string; } | null>;
-  saveUser(emailId: string): Promise<boolean>;
+  saveUser(emailId: string): Promise<void>;
 }
