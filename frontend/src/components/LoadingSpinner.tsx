3e4f473352b3a669195a1aa48d3713b6f0d95a8c
Prerak Jain <prerakjain1007@gmail.com>
2024-05-06 10:28:21 +0530

    fe: minor changes

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -12,10 +12,10 @@ export default function LoadingSpinner({
     <div className="inline-block align-middle mb-0.5 ml-2">
       <div className="">
         {variant === "large" && (
-          <div className="sp-circle w-6 h-6 clear-both my-1.5 mx-auto border-4 border-gray-400 border-t-4 border-t-black"></div>
+          <div className="loading-spinner w-6 h-6 clear-both my-1.5 mx-auto border-4 border-gray-400 border-t-4 border-t-black"></div>
         )}
         {variant === "button" && (
-          <div className="sp-circle w-3.5 h-3.5 align-bottom border-[3px] border-gray-200 border-t-[3px] border-t-gray-500"></div>
+          <div className="loading-spinner w-3 h-3 align-bottom border-[3px] border-gray-200 border-t-[3px] border-t-gray-500"></div>
         )}
       </div>
     </div>
